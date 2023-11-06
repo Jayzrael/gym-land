e104a55fb1bd1761feccac68aa36b45f55cc4ec6
Israel Omole <omolewayeolagd1997@gmail.com>
2023-11-06 10:06:29 +0100

    done with navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+import Nav from "./components/Nav";
+import { SelectedPage } from "./components/types";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
-      <div>Home</div>
+      <div className="app">
+        <Nav selectedPage={selectedPage} setSelectedPage={setSelectedPage} isTopOfPage={isTopOfPage}/>
+      </div>
     </>
   );
 }
